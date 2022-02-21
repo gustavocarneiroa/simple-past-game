c96e7c369126938d22c7c97e93fe843b3da3abbf
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2022-02-20 23:05:43 -0300

    [First commit]

---
+++ verbs.js
@@ -0,0 +1,308 @@
+const verbArray = [{ verb: 'study', type: 'regular', conjugation: 
+`I studied
+you studied
+he/she/it studied
+we studied
+you studied
+they studied` },
+{ verb: 'learn', type: 'regular', conjugation: 
+`I learned
+you learned
+he learned
+we learned
+you learned
+they learned` },
+{ verb: 'walk', type: 'regular', conjugation: 
+`I walked
+you walked
+he walked
+we walked
+you walked
+they walked` },
+{ verb: 'love', type: 'regular', conjugation:
+`I loved
+you loved
+he loved
+we loved
+you loved
+they loved` },
+{ verb: 'close', type: 'regular', conjugation:
+`I closed
+you closed
+he closed
+we closed
+you closed
+they closed` },
+{ verb: 'work', type: 'regular', conjugation:
+`I worked
+you worked
+he worked
+we worked
+you worked
+they worked` },
+{ verb: 'call', type: 'regular', conjugation:
+`I called
+you called
+he called
+we called
+you called
+they called` },
+{ verb: 'Accept', type: 'regular', conjugation: 
+`I accepted
+you accepted
+he accepted
+we accepted
+you accepted
+they accepted` },
+{ verb: 'Agree', type: 'regular', conjugation: 
+`I agreed
+you agreed
+he agreed
+we agreed
+you agreed
+they agreed` },
+{ verb: 'Ask', type: 'regular', conjugation: 
+`I asked
+you asked
+he asked
+we asked
+you asked
+they asked` },
+{ verb: 'Believe', type: 'regular', conjugation: 
+`I believed
+you believed
+he believed
+we believed
+you believed
+they believed` },
+{ verb: 'Call', type: 'regular', conjugation: 
+`I called
+you called
+he called
+we called
+you called
+they called` },
+{ verb: 'Close', type: 'regular', conjugation: 
+`I closed
+you closed
+he closed
+we closed
+you closed
+they closed` },
+{ verb: 'Decide', type: 'regular', conjugation: 
+`I decided
+you decided
+he decided
+we decided
+you decided
+they decided` },
+{ verb: 'Dress', type: 'regular', conjugation: 
+`I dressed
+you dressed
+he dressed
+we dressed
+you dressed
+they dressed` },
+{ verb: 'Die', type: 'regular', conjugation: 
+`I died
+you died
+he died
+we died
+you died
+they died` },
+{ verb: 'Enjoy', type: 'regular', conjugation: 
+`I enjoyed
+you enjoyed
+he enjoyed
+we enjoyed
+you enjoyed
+they enjoyed` },
+{ verb: 'Fix', type: 'regular', conjugation: 
+`I fixed
+you fixed
+he fixed
+we fixed
+you fixed
+they fixed` },
+{ verb: 'Fry', type: 'regular', conjugation: 
+`I fried
+you fried
+he fried
+we fried
+you fried
+they fried` },
+{ verb: 'Need', type: 'regular', conjugation: 
+`I needed
+you needed
+he needed
+we needed
+you needed
+they needed` },
+{ verb: 'Open', type: 'regular', conjugation: 
+`I opened
+you opened
+he opened
+we opened
+you opened
+they opened` },
+{ verb: 'Shop', type: 'regular', conjugation: 
+`I shopped
+you shopped
+he shopped
+we shopped
+you shopped
+they shopped` },
+{ verb: 'Do', type: 'irregular', conjugation: 
+`I did
+you did
+he did
+we did
+you did
+they did` },
+{ verb: 'Go', type: 'irregular', conjugation: 
+`I went
+you went
+he went
+we went
+you went
+they went` },
+{ verb: 'Buy', type: 'irregular', conjugation: 
+`I bought
+you bought
+he bought
+we bought
+you bought
+they bought` },
+{ verb: 'Choose', type: 'irregular', conjugation: 
+`I chose
+you chose
+he chose
+we chose
+you chose
+they chose` },
+{ verb: 'Break', type: 'irregular', conjugation: 
+`I broke
+you broke
+he broke
+we broke
+you broke
+they broke` },
+{ verb: 'Fell', type: 'irregular', conjugation: 
+`I felt
+you felt
+he felt
+we felt
+you felt
+they felt` },
+{ verb: 'Hear', type: 'irregular', conjugation: 
+`I heard
+you heard
+he heard
+we heard
+you heard
+they heard` },
+{ verb: 'Build', type:'irregular' , conjugation: 
+`I built
+you built
+he built
+we built
+you built
+they built` },
+{ verb: 'Bring', type: 'irregular', conjugation: 
+`I brought
+you brought
+he brought
+we brought
+you brought
+they brought` },
+{ verb: 'Come', type: 'irregular', conjugation: 
+`I came
+you came
+he came
+we came
+you came
+they came` },
+{ verb: 'Drive', type: 'irregular', conjugation: 
+`I drove
+you drove
+he drove
+we drove
+you drove
+they drove` },
+{ verb: 'Drink', type: 'irregular', conjugation: 
+`I drank
+you drank
+he drank
+we drank
+you drank
+they drank` },
+{ verb: 'Fall', type: 'irregular', conjugation: 
+`I fell
+you fell
+he fell
+we fell
+you fell
+they fell` },
+{ verb: 'Find', type: 'irregular', conjugation: 
+`I found
+you found
+he found
+we found
+you found
+they found` },
+{ verb: 'Give', type: 'irregular', conjugation: 
+`I gave
+you gave
+he gave
+we gave
+you gave
+they gave` },
+{ verb: 'Have', type: 'irregular', conjugation: 
+`I had
+you had
+he had
+we had
+you had
+they had` },
+{ verb: 'Keep', type: 'irregular', conjugation: 
+`I kept
+you kept
+he kept
+we kept
+you kept
+they kept` },
+{ verb: 'Know', type: 'irregular', conjugation: 
+`I knew
+you knew
+he knew
+we knew
+you knew
+they knew` },
+{ verb: 'Make', type: 'irregular', conjugation: 
+`I made
+you made
+he made
+we made
+you made
+they made` },
+{ verb: 'Say', type: 'irregular', conjugation: 
+`I said
+you said
+he said
+we said
+you said
+they said` },
+{ verb: 'Speak', type: 'irregular', conjugation: 
+`I spoke
+you spoke
+he spoke
+we spoke
+you spoke
+they spoke` },
+{ verb: 'Write', type: 'irregular', conjugation: 
+`I wrote
+you wrote
+he wrote
+we wrote
+you wrote
+they wrote` }]
